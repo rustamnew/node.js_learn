78e6ed34f0d1ad35b16794aca16f36a8fb1e1114
rustamnew <1rustamnew1@gmail.com>
2024-03-06 19:39:59 +0300

    Базовый сервер

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
-const fs = require('fs')
-const path = require('path')
+const http = require('http')
 
-//Чтение
-const pathReadFile = './data/data1.json'
+http.createServer( function(req, res) {
+    res.setHeader('Content-type', 'text/html; charset=utf-8;')
 
-const data = fs.readFileSync(pathReadFile, { //получение текстового содержимого
-    encoding: 'utf8', //кодировка
-    //flag: 'r' //по умолчанию - для чтения
-})
-console.log(data)
-console.log(data.split('\r\n')) //разбить по строкам
-console.log(JSON.parse(data)) //получить объект
+    console.log(req.url)
+    console.log(req.method)
+    console.log('')
 
-
-
-//Запись
-const data_obj = JSON.parse(data)
-data_obj.user.count++
-
-console.log(data_obj)
-
-const edited_data_string = JSON.stringify(data_obj, null, 4)
-
-fs.writeFileSync('./data/data_temp.json', edited_data_string, { //Синхронная
-    encoding: 'utf8',
-    //flag: 'a', //Добавить
-    flag: 'w', //Переписать
-})
-
-fs.writeFile('./data/data_temp1.json', edited_data_string, /*{flag: 'a'}, Необязательный параметр с конфигом*/ function(error){ //Асинхронная
-    if(error){  // если ошибка
-        return console.log(error);
-    }
-    console.log("Файл успешно записан");
-});
+    res.write(req.url);
+    res.write('<br>');
+    res.write(req.method);
+    
+    res.end()
+}).listen(3500)
