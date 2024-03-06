4ca6544fe504627e66959b85281f36b724c31900
rustamnew <1rustamnew1@gmail.com>
2024-03-06 16:28:13 +0300

    Чтение и запись файла

--- index.js
+++ index.js
@@ -1,52 +1,36 @@
 const fs = require('fs')
 const path = require('path')
 
-//Абсолютный путь 
-const absolutePath = path.join(__dirname, './data/data1.json');
-//console.log(__dirname)
-console.log(absolutePath)
+//Чтение
+const pathReadFile = './data/data1.json'
 
-//Проверка существования папки
-function checkDir(pathDir = './data') {
-    if (fs.existsSync(pathDir)) {
-        console.log(true)
-    } else {
-        console.log(false)
+const data = fs.readFileSync(pathReadFile, { //получение текстового содержимого
+    encoding: 'utf8', //кодировка
+    //flag: 'r' //по умолчанию - для чтения
+})
+console.log(data)
+console.log(data.split('\r\n')) //разбить по строкам
+console.log(JSON.parse(data)) //получить объект
+
+
+
+//Запись
+const data_obj = JSON.parse(data)
+data_obj.user.count++
+
+console.log(data_obj)
+
+const edited_data_string = JSON.stringify(data_obj, null, 4)
+
+fs.writeFileSync('./data/data_temp.json', edited_data_string, { //Синхронная
+    encoding: 'utf8',
+    //flag: 'a', //Добавить
+    flag: 'w', //Переписать
+})
+
+fs.writeFile('./data/data_temp1.json', edited_data_string, /*{flag: 'a'}, Необязательный параметр с конфигом*/ function(error){ //Асинхронная
+    if(error){  // если ошибка
+        return console.log(error);
     }
-}
-
-//Инфо о файле
-function checkFile(pathFile = './data/data.json') { 
-    const fileInfo = fs.statSync(pathFile)
-
-    if (fs.existsSync(pathFile)) {
-        console.log(fileInfo.size + ' Байт')
-        //console.log(path.basename(pathFile))
-        //console.log(path.extname(pathFile))
-        //console.log(path.dirname(pathFile))
-        console.log(path.parse(pathFile))
-    } else {
-        console.log(false)
-    }
-}
-
-//Инфо о файлах в папке
-function checkDirFiles(pathDir = './data') {
-    if (fs.existsSync(pathDir)) {
-        const all_files = fs.readdirSync(pathDir)
-
-        all_files.forEach( (item) => {
-            const item_path = pathDir + '/' + item
-
-            checkFile(item_path)
-        });
-    } else {
-        console.log(false)
-    }
-}
-
-checkDir()
-console.log('-----')
-checkFile()
-console.log('-----')
-checkDirFiles()
+    console.log("Файл успешно записан");
+});
